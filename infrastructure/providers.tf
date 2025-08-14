5c7043566bc40cf0ecf7f5aec5a5f9286a8d8741
JonatanMendez29 <jonatanmendez29@gmail.com>
2025-08-14 16:54:05 -0600

    First commit for phase 1: IaC. This file configure a AWS Provider

---
+++ infrastructure/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+  default_tags {
+    tags = {
+      Project = "fraud-detection"
+    }
+  }
+}
