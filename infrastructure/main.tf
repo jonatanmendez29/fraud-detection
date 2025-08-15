e2cc3f748ff196783a72d965177e818b03c74983
JonatanMendez29 <jonatanmendez29@gmail.com>
2025-08-14 20:25:47 -0600

    Update .gitignore to add Terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,16 +1,9 @@
-provider "aws" {
-  region = var.aws_region
-}
 module "networking" {
   source = "./modules/networking"
 }
 module "storage" {
   source = "./modules/storage"
-  project_name = var.project_name
 }
 module "compute" {
   source = "./modules/compute"
-  project_name = var.project_name
-  vpc_id = module.networking.vpc_id
-  private_subnet_ids = module.networking.private_subnet_ids
 }
